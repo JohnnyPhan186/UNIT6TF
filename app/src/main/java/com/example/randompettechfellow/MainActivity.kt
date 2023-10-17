b07055d62a1ec970295b434272497ee48fa3d8aa
JohnnyPhan186 <phan.johnny036@gmail.com>
2023-10-16 21:32:39 -0400

    unit6TF

--- app/src/main/java/com/example/randompettechfellow/MainActivity.kt
+++ app/src/main/java/com/example/randompettechfellow/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
@@ -12,15 +14,18 @@ import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
     var petImageURL = ""
+    private lateinit var petList: MutableList<String>
+    private lateinit var rvPets: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("petImageURL", "pet image URL set")
 
 
-        val button = findViewById<Button>(R.id.petButton)
-        val imageView =findViewById<ImageView>(R.id.petImageView)
-        getNextImage(button, imageView)
+
+        petList = mutableListOf()
+        rvPets = findViewById(R.id.pet_list)
+        getDogImageURL()
     }
 
     private fun getNextImage (button: Button, imageView: ImageView) {
@@ -30,16 +35,24 @@ class MainActivity : AppCompatActivity() {
             Glide.with(this)
                 .load(petImageURL)
                 .fitCenter()
+                .centerCrop()
                 .into(imageView)
 
         }
     }
     private fun getDogImageURL() {
         val client = AsyncHttpClient()
-        client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
+        client["https://dog.ceo/api/breeds/image/random/20", object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
                 Log.d("Dog", "response successful$json")
-                petImageURL = json.jsonObject.getString("message")
+                val petImageArray = json.jsonObject.getJSONArray("message")
+                for (i in 0 until petImageArray.length()) {
+                    petList.add(petImageArray.getString(i))
+                }
+                val adapter = PetAdapter(petList)
+                rvPets.adapter = adapter
+                rvPets.layoutManager = LinearLayoutManager(this@MainActivity)
+
             }
 
             override fun onFailure(
